90b3f88717e3dfd82ac5a457ba2fb7f3489f4b4f
Abbas5901E <71883084+Abbas5901E@users.noreply.github.com>
2020-11-28 15:41:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var newball;
+var database;
+var position;
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(500,500);
+
+    newball = createSprite(250,250,10,10);
+    newball.shapeColor = "red";
+    var newballposition = database.ref('ball/position');
+    newballposition.on("value", readposition, showerror);
+}
+
+function draw(){
+    background("white");
+
+    if(position !== undefined){
+        
+    
+    if(keyDown(LEFT_ARROW)){
+        writeposition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writeposition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writeposition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writeposition(0,+1);
+    }
+    drawSprites();
+
+}
+}
+
+function writeposition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x, 
+        'y' : position.y + y
+    })
+}
+
+function readposition(data){
+    position = data.val();
+    newball.x = position.x;
+    newball.y = position.y;
+}
+
+function showerror(){
+    console.log("error is there");
+}
